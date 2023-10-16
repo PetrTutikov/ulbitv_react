d030d8b5597e5dda537b8e09cba7efb83a9a4f79
obold <p240188@gmail.com>
2023-10-16 20:14:53 +0500

    инпут не управляемый компонент и управляемый

--- src/App.js
+++ src/App.js
@@ -1,6 +1,8 @@
-import React, { useState } from 'react'
+import React, { useRef, useState } from 'react'
 import './styles/App.css'
 import PostList from './components/PostList'
+import MyButton from './components/UI/button/MyButton'
+import MyInput from './components/UI/input/MyInput'
 
 function App() {
   const [posts, setPosts] = useState([
@@ -8,12 +10,28 @@ function App() {
     {id: 2, title: 'Javascript 2', body: 'Description'},
     {id: 3, title: 'Javascript 3', body: 'Description'}
   ])
+  const [title, setTitle] = useState('')
+  const bodyInputRef = useRef()
+  const addNewPost = (e) => {
+    e.preventDefault()
+    console.log(title)
+    console.log(bodyInputRef.current.value)
+  }
+
   return (
     <div className="App">
       <form>
-        <input type='text' placeholder='Название поста'/>
-        <input type='text' placeholder='Описание поста'/>
-        <button>Создать пост</button>
+        <MyInput value={title}
+                 onChange={e => setTitle(e.target.value)}
+                 type='text'
+                 placeholder='Название поста'
+        />
+        <MyInput
+          ref={bodyInputRef}
+          type='text'
+          placeholder='Описание поста'
+        />
+        <MyButton onClick={addNewPost}>Создать пост</MyButton>
       </form>
       <PostList posts={posts} title='Список постов 1'/>
     </div>
--- src/components/UI/button/MyButton.jsx
+++ src/components/UI/button/MyButton.jsx
@@ -1,5 +1,12 @@
-export const MyButton = () => {
+import React from 'react';
+import classes from './MyButton.module.css';
+
+const MyButton = ({children, ...props}) => {
   return (
-    <></>
+    <button {...props} className={classes.myBtn}>
+      {children}
+    </button>
   )
 }
+
+export default MyButton
