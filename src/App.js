78d2a2917394207b17d4ac2e597286411ac423b9
obold <p240188@gmail.com>
2023-10-17 14:52:32 +0500

    создание собственного хука для поиска и фильттрации списка постов
    https://youtu.be/GNrdg3PzpJQ?t=5781

--- src/App.js
+++ src/App.js
@@ -3,30 +3,16 @@ import './styles/App.css'
 import PostList from './components/PostList'
 import PostForm from './components/PostForm'
 import { useState } from 'react'
-import { useMemo } from  'react'
 import PostFilter from './components/PostFilter'
 import MyModal from './components/UI/MyModal/MyModal'
 import MyButton from './components/UI/button/MyButton'
+import { usePosts } from './hooks/usePosts'
 
 function App() {
-  const [posts, setPosts] = useState([
-    {id: 1, title: 'aa', body: 'bb'},
-    {id: 2, title: 'bb', body: 'zz'},
-    {id: 3, title: 'zz', body: 'aa'}
-  ])
+  const [posts, setPosts] = useState([])
   const [filter, setFilter] = useState({sort: '', query: ''});
   const [modal, setModal] = useState(false);
-
-  const sortedPosts = useMemo(() => {
-    if(filter.sort) {
-      return [...posts.sort((a, b) => a[filter.sort].localeCompare(b[filter.sort]))];
-    }
-    return posts;
-  }, [filter.sort, posts])
-
-  const sortedAndSearchedPost = useMemo(() => {
-    return sortedPosts.filter(post => post.title.toLowerCase().includes(filter.query.toLowerCase()))
-  }, [filter.query, sortedPosts])
+  const  sortedAndSearchedPost = usePosts(posts, filter.sort, filter.query);
 
   const createPost = (newPost) => {
     setPosts([...posts, newPost])
